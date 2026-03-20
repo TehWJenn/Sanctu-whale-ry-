4eb85110732abae690aac66d374193c2b7728740
TehWJenn <tehw0013@gmail.com>
2026-03-20 03:30:58 +0200

    new updates

--- Assets/Scripts/Playermovement.cs
+++ Assets/Scripts/Playermovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Playermovement : MonoBehaviour
 {
-    [SerializeField] float speed = 2f;
+    [SerializeField] float speed = 3f;
     Vector2 lastClickedPos;
     bool moving;
 
